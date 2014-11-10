2a96fa0a99faed20238e9e94c4fcec6ac93f0dc7
j4chaves <jacobjc3@gmail.com>
2014-11-10 18:07:51 -0500

    First real commit

--- Character.py
+++ Character.py
@@ -1,1 +1,24 @@
 __author__ = 'Jacob'
+"""
+2D Game Programming
+Project 3
+Character Class
+
+This is a parent class for the Enemy and Defender classes.
+"""
+
+from kivy.uix.image import Image
+from kivy.properties import StringProperty
+
+class Character:
+
+    #Variables
+    health = 0
+    picture = StringProperty(None)
+
+    def init(self):
+        pass
+
+    def loadImages(self):
+        pass
+
---
+++ Enemy.py
@@ -0,0 +1,11 @@
+__author__ = 'Jacob'
+"""
+2D Game Programming
+Project 3
+Character Class
+
+This is the Enemy class.  It is the computer controlled characters
+that will be traveling from the right side of the screen to the left
+and attack the Defenders.
+"""
+
