e2280d19b74a1576d5a7dd2f08f6a5c6be8fbe6d
j4chaves <jacobjc3@gmail.com>
2014-11-11 19:48:04 -0500

    fgfcgfcgb

--- Game.py
+++ Game.py
@@ -12,12 +12,13 @@ from kivy.uix.widget import Widget
 from kivy.clock import Clock
 
 class Game(Widget):
-    pass
+    def update(self):
+        pass
 
 class GameApp(App):
     game = Game()
-    Clock.shedule_update(game.update, 1.0/60.0)
-    return game
+    Clock.schedule_interval(game.update, 1.0/60.0)
+    #return game
 
 if __name__ == '__main__':
-    GameApp.run()
+    GameApp().run()
